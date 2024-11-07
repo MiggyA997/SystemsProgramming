dd8f4ccfcd411a142257cffbdd8f48e09f352629
Miguel Abreu <97925946+MiggyA997@users.noreply.github.com>
2024-11-07 15:30:59 -0500

    Add files via upload

---
+++ Assignment1/pa1.h
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+#ifndef PA1_H
+#define PA1_H
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 1000
+
+int is_palindrome(char str[]);
+int is_singleton(char str[]);
+int is_arithmetic(char str[]);
+int is_reverse_arithmetic(char str[]);
+int is_balanced_tripartite(char str[]);
+int is_balanced_bipartite(char str[]);
+
+#endif
